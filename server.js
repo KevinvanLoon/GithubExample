b955314fcb4d1cb7348805dc2ec58e4c2fdff4fa
KevinvanLoon <kloon3@avans.nl>
2018-04-23 15:04:16 +0200

    Revert "heroku"
    This reverts commit 5937e26f3ab0872703641b4270339793fdb55658.

--- server.js
+++ server.js
@@ -5,18 +5,22 @@ const app = express(
 );
 
 app.all('*', (reg, res, next)=>{
-    next();
+   next();
 });
 
+<<<<<<< HEAD
 const port = 8080;
 app.listen(port, ()=>{
+=======
+app.listen(8080, ()=>{
+>>>>>>> parent of 5937e26... heroku
     console.log("The fun starts dfgsdfh8080");
 });
 
 app.get('/hello', (reg, res, next)=>{
-    res.status(200).json({
-        'msg' : 'Hello xD'
-    });
+   res.status(200).json({
+       'msg' : 'Hello xD'
+   });
 });
 
 module.exports = app;
