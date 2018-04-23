bc8fddb145708fca0d0e580eeb9bdacb2723fc1b
KevinvanLoon <kloon3@avans.nl>
2018-04-23 14:39:41 +0200

    server postman

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.all('*', (reg, res, next)=>{
 });
 
 app.listen(8080, ()=>{
-    console.log("The fun starts 8080");
+    console.log("The fun starts dfgsdfh8080");
+});
+
+app.get('/hello', (reg, res, next)=>{
+   res.status(200).json({
+       'msg' : 'Hello xD'
+   });
 });
 
 module.exports = app;
